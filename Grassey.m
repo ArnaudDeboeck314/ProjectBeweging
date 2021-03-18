bdc320b2e530026ae598e6bb245975529b42e918
Seppe Vilain <seppe.vilain@student.kuleuven.be>
2021-03-18 16:04:08 +0100

    Clean up of the code + extra plots

--- Grassey.m
+++ Grassey.m
@@ -1,3 +1,27 @@
+% #####################################################
+%
+% Linkage system: Linkage system of Ghassaei
+% 
+% Seppe Vilain, Arnoud Deboeck
+% 
+% #####################################################
+
+% start with empty plots
+clear 
+close all
+
+% #####################################################
+% Data initialisation
+% #####################################################
+
+% program parameters 
+% (1 if the plot needs to be done 0 if it doesn't)
+plot_kin = 1;
+plot_dyn = 1;
+plot_mov = 1;
+
+
+% kinematic parameters (link lengths) 
 r1 = 53;
 r2 = 26;
 r3 = 56;
@@ -11,6 +35,7 @@ r10 = 97;
 r11 = 77;
 r12 = 56;
 
+% link weights
 m2 = r2*1.76;
 m3 = r3*1.76;
 m4 = r4*1.76;
@@ -23,6 +48,7 @@ m10 = r10*1.76;
 m11 = r11*1.76;
 m12 = r12*1.76;
 
+% link inertia 
 I2 = m2*r2^2/12 + m2*r2/2;
 I3 = m3*r3^2/12;
 I4 = m4*r4^2/12;
@@ -35,48 +61,59 @@ I10 = m10*r10^2/12;
 I11 = m11*r11^2/12;
 I12 = m12*r12^2/12;
 
-t1 = 0.085;
+
+% #####################################################
+% 1) Kinematic analyse
+% #####################################################
+
+
 
 omega = 0.15;
 t_begin = 0;
 t_end = round(2*2*pi/omega,0);
-Ts = 0.05;
+Ts = 0.005;
 t = [t_begin:Ts:t_end]';
 
-t2=1.4+omega*t;               
-dt2=omega+0*t;
-ddt2 = 0;
+%t1=cte
+t1 = 0.085;
 
+%t3_init untill t12_init
 t_init = [1.8 1.1 2.8 2.9 1.1 1.9 2.7 0.4 1.3 2.3];
 
-[t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,dt3,dt4,dt5,dt6,dt7,dt8,dt9,dt10,dt11,dt12,ddt3,ddt4,ddt5,ddt6,ddt7,ddt8,ddt9,ddt10,ddt11,ddt12] = kinematics_grassey(r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,t1,t2,dt2,ddt2,t_init,t);
+%t2 "changing" corner
+t2=1.4+omega*t; 
+dt2=omega+0*t; % first derivative
+ddt2 = 0; % second derivative
 
+% calculate the kinematics
+[t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,dt3,dt4,dt5,dt6,dt7,dt8,dt9,dt10,dt11,dt12,ddt3,ddt4,ddt5,ddt6,ddt7,ddt8,ddt9,ddt10,...
+    ddt11,ddt12] = kinematics_grassey(r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,t1,t2,dt2,ddt2,t_init,t,plot_kin);
+
+% #####################################################
+% 2) Control of kinematic calculation 
+% #####################################################
+
+% #####################################################
+% 3) Dynamic calculation 
+% #####################################################
 [F1_2_x,F1_2_y,F2_12_x,F2_12_y,F2_3_x,F2_3_y,F3_4_x,F3_4_y,F3_5_x,F3_5_y,F4_6_x,F4_6_y,F6_7_x,F6_7_y,F6_8_x,F6_8_y,F8_9_x, ...
-          F8_9_y,F8_10_x,F8_10_y,F12_10_x,F12_10_y,F12_11_x,F12_11_y,F1_5_x,F1_5_y,F1_7_x,F1_7_y,F1_9_x,F1_9_y,F1_11_x,F1_11_y,M_2,M_2c] = grassey_dynamics(t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,ddt2,ddt3,ddt4,ddt5,ddt6,ddt7,ddt8,ddt9,ddt10,ddt11,ddt12,dt2,dt3,dt4,dt5,dt6,dt7,dt8,dt9,dt10,dt11,dt12, ...
-                           r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,I2,I3,I4,I5,I6,I7,I8,I9,I10,I11,I12,t);
+          F8_9_y,F8_10_x,F8_10_y,F12_10_x,F12_10_y,F12_11_x,F12_11_y,F1_5_x,F1_5_y,F1_7_x,F1_7_y,F1_9_x,F1_9_y,F1_11_x,F1_11_y,...
+          M_2,M_2c] = ...
+          grassey_dynamics(t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,ddt2,ddt3,ddt4,ddt5,ddt6,ddt7,ddt8,ddt9,ddt10,ddt11,...
+          ddt12,dt2,dt3,dt4,dt5,dt6,dt7,dt8,dt9,dt10,dt11,dt12,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,m2,m3,m4,m5,m6,m7,m8,m9,m10,...
+          m11,m12,I2,I3,I4,I5,I6,I7,I8,I9,I10,I11,I12,t);
                        
-                       
-index = 1; 
-    
-    A = 0;
-    B = r1*exp(j*t1);
-    C = B + r2*exp(j*t2(index));
-    H = C + r12*exp(j*(t12(index)));
-    F = A + r7 * exp(j*(t7(index)+pi));
-    E = F + r6 * exp(j*(t6(index)+pi));
-    D = E + r4 * exp(j*(t4(index)));
-    G = A + r9 * exp(j*(t9(index)));
-    
-    
-    figure
-    assembly=[A, B, C, H, A, F, E, D, A, B, C, D, A, G, F, G, H];
-    plot(real(assembly),imag(assembly),'ro-')
-    xlabel('[m]')
-    ylabel('[m]')
-    title('assembly')
-    axis equal
+% #####################################################
+% 4) Control of dynamic calculation 
+% #####################################################
+                 
 
-    
+
+% #####################################################
+% 5) Movie 
+% #####################################################
+if plot_mov     
 figure
 load Grassey_movie Movie
 movie(Movie)
+end
--- kinematics_grassey.m
+++ kinematics_grassey.m
@@ -1,6 +1,13 @@
+% #####################################################
+%
+% Linkage system: Linkage system of Ghassaei
+% 
+% Seppe Vilain, Arnoud Deboeck
+% 
+% #####################################################
 function [t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,dt3,dt4,dt5,dt6,dt7,dt8,dt9,dt10,...
             dt11,dt12,ddt3,ddt4,ddt5,ddt6,ddt7,ddt8,ddt9,ddt10,ddt11,ddt12] ...
-            = kinematics_grassey(r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,t1,t2,dt2,ddt2,t_init,t)
+            = kinematics_grassey(r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,t1,t2,dt2,ddt2,t_init,t,plot_kin)
 
 t3 = zeros(size(t));
 t4 = zeros(size(t));
@@ -35,15 +42,33 @@ ddt10 = zeros(size(t));
 ddt11 = zeros(size(t));
 ddt12 = zeros(size(t));
 
+% fsolve options (help fsolve, help optimset)
 optim_options = optimset('Display','off');
 
-Ts = t(2) - t(1);     
-t_size = size(t,1); 
+% *** loop over positions ***
+Ts = t(2) - t(1);    % timestep 
+t_size = size(t,1);  % number of simulation steps
 
 for k=1:t_size
     
-    x = fsolve('Loop_closure_Grassey',t_init',optim_options,t2(k),r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,t1);
+     % *** position analysis ***
     
+    % fsolve solves the non-linear set of equations
+    % loop closure equations: see loop_closure_eqs.m
+    % argument loop_closure_eqs: file containing closure equations
+    % argument [..]': initial values of unknown angles phi3 and phi4
+    % argument optim options: parameters for fsolve
+    % argument phi2(k): input angle phi2 for which we want to calculate the unknown angles phi3 and phi4
+    % argument a1 ... phi1: constants
+    % return value x: solution for the unknown angles phi3 and phi4
+    % return exitflag: indicates convergence of algorithm
+    [x, ~, exitflag] = fsolve('Loop_closure_Grassey',t_init',optim_options,t2(k),r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,t1);
+    
+    if (exitflag ~= 1)
+        disp 'The fsole exit flag was not 1, probably no convergence!'
+    end
+    
+    % save the results of the fsolve above
     t3(k) = x(1);
     t4(k) = x(2);
     t5(k) = x(3);
@@ -54,6 +79,8 @@ for k=1:t_size
     t10(k) = x(8);
     t11(k) = x(9);
     t12(k) = x(10);
+    
+    % ###Velocity Analysis###
     
     A = [0, 0, 0, 0, 0, 0, 0, 0, r11*sin(t11(k)), -r12*sin(t12(k));
          0, 0, 0, 0, 0, 0, 0, 0, -r11*cos(t11(k)), r12*cos(t12(k));
@@ -79,6 +106,7 @@ for k=1:t_size
     
     x = A\B;
     
+    %results
     dt3(k) = x(1);
     dt4(k) = x(2);
     dt5(k) = x(3);
@@ -89,6 +117,8 @@ for k=1:t_size
     dt10(k) = x(8);
     dt11(k) = x(9);
     dt12(k) = x(10);
+    
+    % ###Acceleration Analysis###
     
     A = [0, 0, 0, 0, 0, 0, 0, 0, r11*sin(t11(k)), -r12*sin(t12(k));
          0, 0, 0, 0, 0, 0, 0, 0, -r11*cos(t11(k)), r12*cos(t12(k));
@@ -114,8 +144,12 @@ for k=1:t_size
          -r3*sin(t3(k))*dt3(k)^2+r5*sin(t5(k))*dt5(k)^2+r2*sin(t2(k))*dt2(k)^2-r2*cos(t2(k))*ddt2;
          ];
     
+
+
+     
     x = A\B;
     
+    % results
     ddt3(k) = x(1);
     ddt4(k) = x(2);
     ddt5(k) = x(3);
@@ -127,6 +161,8 @@ for k=1:t_size
     ddt11(k) = x(9);
     ddt12(k) = x(10);
     
+    % change the initial values by the new calculated initial values for
+    % the next iteration step.
     t_init = [t3(k) + Ts*dt3(k), t4(k) + Ts*dt4(k), t5(k) + Ts*dt5(k), t6(k) + Ts*dt6(k), t7(k) + Ts*dt7(k),
               t8(k) + Ts*dt8(k), t9(k) + Ts*dt9(k), t10(k) + Ts*dt10(k), t11(k) + Ts*dt11(k), t12(k) + Ts*dt12(k)]; 
     
@@ -182,3 +218,165 @@ end
 % save movie
 save Grassey_movie Movie
 close(10)
+
+% #### PLOTS ####
+
+if plot_kin % only plot when asked (option  for time purpose)
+    index = 1; 
+    
+    A = 0;
+    B = r1*exp(j*t1);
+    C = B + r2*exp(j*t2(index));
+    H = C + r12*exp(j*(t12(index)));
+    F = A + r7 * exp(j*(t7(index)+pi));
+    E = F + r6 * exp(j*(t6(index)+pi));
+    D = E + r4 * exp(j*(t4(index)));
+    G = A + r9 * exp(j*(t9(index)));
+    
+    
+    figure
+    assembly=[A, B, C, H, A, F, E, D, A, B, C, D, A, G, F, G, H];
+    plot(real(assembly),imag(assembly),'ro-')
+    xlabel('[m]')
+    ylabel('[m]')
+    title('assembly')
+    axis equal
+    
+    %###### corner plot #####
+    figure
+    subplot(11,1,1)
+    plot(t,t2)
+    ylabel('\phi_2 [rad]')
+    
+    subplot(11,1,2)
+    plot(t,t3)
+    ylabel('\phi_3 [rad]')
+    
+    subplot(11,1,3)
+    plot(t,t4)
+    ylabel('\phi_4 [rad]')
+    
+    subplot(11,1,4)
+    plot(t,t5)
+    ylabel('\phi_5 [rad]')
+    
+    subplot(11,1,5)
+    plot(t,t6)
+    ylabel('\phi_6 [rad]')
+    
+    subplot(11,1,6)
+    plot(t,t7)
+    ylabel('\phi_7 [rad]')
+    
+    subplot(11,1,7)
+    plot(t,t8)
+    ylabel('\phi_8 [rad]')
+    
+    subplot(11,1,8)
+    plot(t,t9)
+    ylabel('\phi_9 [rad]')
+    
+    subplot(11,1,9)
+    plot(t,t10)
+    ylabel('\phi_{10} [rad]')
+    
+    subplot(11,1,10)
+    plot(t,t11)
+    ylabel('\phi_{11} [rad]')
+    
+    subplot(11,1,11)
+    plot(t,t12)
+    ylabel('\phi_{12} [rad]')
+    
+    %###### corner velocity plot #####
+    figure 
+     subplot(11,1,1)
+    plot(t,dt2)
+    ylabel('d\phi_2 [rad/s]')
+    
+    subplot(11,1,2)
+    plot(t,dt3)
+    ylabel('d\phi_3 [rad/s]')
+    
+    subplot(11,1,3)
+    plot(t,dt4)
+    ylabel('d\phi_4 [rad/s]')
+    
+    subplot(11,1,4)
+    plot(t,dt5)
+    ylabel('d\phi_5 [rad/s]')
+    
+    subplot(11,1,5)
+    plot(t,dt6)
+    ylabel('d\phi_6 [rad/s]')
+    
+    subplot(11,1,6)
+    plot(t,dt7)
+    ylabel('d\phi_7 [rad/s]')
+    
+    subplot(11,1,7)
+    plot(t,dt8)
+    ylabel('d\phi_8 [rad/s]')
+    
+    subplot(11,1,8)
+    plot(t,dt9)
+    ylabel('d\phi_9 [rad/s]')
+    
+    subplot(11,1,9)
+    plot(t,dt10)
+    ylabel('d\phi_{10} [rad/s]')
+    
+    subplot(11,1,10)
+    plot(t,dt11)
+    ylabel('d\phi_{11} [rad/s]')
+    
+    subplot(11,1,11)
+    plot(t,dt12)
+    ylabel('d\phi_{12} [rad/s]')
+    
+    %###### corner acceleration plot #####
+    figure 
+    subplot(11,1,1)
+    plot(t,ddt2)
+    ylabel('dd\phi_2 [rad/s^2]')
+    
+    subplot(11,1,2)
+    plot(t,ddt3)
+    ylabel('dd\phi_3 [rad/s^2]')
+    
+    subplot(11,1,3)
+    plot(t,ddt4)
+    ylabel('dd\phi_4 [rad/s^2]')
+    
+    subplot(11,1,4)
+    plot(t,ddt5)
+    ylabel('dd\phi_5 [rad/s^2]')
+    
+    subplot(11,1,5)
+    plot(t,ddt6)
+    ylabel('dd\phi_6 [rad/s^2]')
+    
+    subplot(11,1,6)
+    plot(t,ddt7)
+    ylabel('dd\phi_7 [rad/s^2]')
+    
+    subplot(11,1,7)
+    plot(t,ddt8)
+    ylabel('dd\phi_8 [rad/s^2]')
+    
+    subplot(11,1,8)
+    plot(t,ddt9)
+    ylabel('dd\phi_9 [rad/s^2]')
+    
+    subplot(11,1,9)
+    plot(t,dt10)
+    ylabel('dd\phi_{10} [rad/s^2]')
+    
+    subplot(11,1,10)
+    plot(t,ddt11)
+    ylabel('dd\phi_{11} [rad/s^2]')
+    
+    subplot(11,1,11)
+    plot(t,ddt12)
+    ylabel('dd\phi_{12} [rad/s^2]')
+end
