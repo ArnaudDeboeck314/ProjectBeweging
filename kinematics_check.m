87fda6670fd4b73f4bb4e4543cf291f280ef25bf
Seppe Vilain <seppe.vilain@student.kuleuven.be>
2021-03-18 12:10:04 +0100

    second

---
+++ kinematics_check.m
@@ -0,0 +1,27 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Linkage system: Linkage System of Ghassaei 
+%
+% Seppe Vilain, Arnoud Deboeck
+%
+% Kinematic analysis check of position, velocity and acceleration
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function  kinematics_check(t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,dt3,dt4,dt5,dt6,dt7,dt8,dt9,dt10,...
+            dt11,dt12,ddt3,ddt4,ddt5,ddt6,ddt7,ddt8,ddt9,ddt10,ddt11,ddt12)
+        
+%%%%%%%%%%%%
+% Position %
+%%%%%%%%%%%%
+
+% CALCULATION OF POSITION FROM DIFFERENT POINTS IN COMPLEX NOTATION
+
+% initialize iteration variables
+
+
+        
+        
+end
+
