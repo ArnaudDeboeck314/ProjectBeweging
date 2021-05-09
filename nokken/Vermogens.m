ea99a3c4b2fba9e9f80611644f09918d694fffa3
ArnaudDeboeck314 <arnaud.deboeck@student.kuleuven.be>
2021-05-09 20:02:47 +0200

    toevoeging van docs

---
+++ nokken/Vermogens.m
@@ -0,0 +1,15 @@
+load('exc_geen_veer_17,25.mat');
+
+P1 = zeros(36000);
+
+for i = 1:36000
+    P1(i) = normalforce_tot(i)*sin(pressure_angle(i))*(60 + S(i))*w;
+end
+
+load('exc_9mm_veer_17,25.mat');
+
+P2 = zeros(36000);
+
+for i = 1:36000
+    P2(i) = normalforce_tot(i) * cos(pressure_angle(i)) * (tan(pressure_angle(i)) * (sqrt((60^2) - (exc^2)) + S(i)) + exc) * w;
+end
