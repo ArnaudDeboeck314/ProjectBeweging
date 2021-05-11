72152bd051531909e7012a3c17b44bd32535ad52
ArnaudDeboeck314 <arnaud.deboeck@student.kuleuven.be>
2021-05-11 18:31:51 +0200

    Update Vermogens.m

--- nokken/Vermogens.m
+++ nokken/Vermogens.m
@@ -1,6 +1,6 @@
 load('exc_geen_veer_17,25.mat');
 
-P1 = zeros(36000);
+P1 = zeros(1,36000);
 
 for i = 1:36000
     P1(i) = normalforce_tot(i)*sin(pressure_angle(i))*(60 + S(i))*w;
@@ -8,7 +8,7 @@ end
 
 load('exc_9mm_veer_17,25.mat');
 
-P2 = zeros(36000);
+P2 = zeros(1,36000);
 
 for i = 1:36000
     P2(i) = normalforce_tot(i) * cos(pressure_angle(i)) * (tan(pressure_angle(i)) * (sqrt((60^2) - (exc^2)) + S(i)) + exc) * w;
